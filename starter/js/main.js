576a42fcd00ce6c79c98f7f2f97a84975c26dfe5
kerrongordon <kgpsounds.com@gmail.com>
2019-10-21 16:34:14 -0400

    added moving image

--- starter/js/main.js
+++ starter/js/main.js
@@ -1,7 +1,24 @@
 
+window.onload=function() { 
+
 const submit = document.getElementById('submit');
+const hero = document.getElementById('hero');
 
-submit.addEventListener('click', function(even) {
-    even.preventDefault()
-});
+    if (submit) {
+        submit.addEventListener('click', function(even) {
+            even.preventDefault()
+        });
+    }
 
+
+    if (hero) {
+        console.log(hero);
+        
+        hero.addEventListener('mousemove', function(even){
+            console.log('x ', even.screenX / 10 );
+            console.log('y ', even.screenY / 10 );
+
+            hero.style.backgroundPosition = `0px ${-even.screenY / 10}px`
+        })
+    }
+};
