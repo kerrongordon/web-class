8a328d7dea95f231419d4c87b27eeea65898778d
kerron <kgpsounds.com@gmail.com>
2019-11-11 15:48:11 -0400

    added the about page and an API also

--- starter/js/main.js
+++ starter/js/main.js
@@ -1,26 +1,64 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 8 */
 
 window.onload = function() { 
 
 const form = document.getElementById('form');
 
-const runForm = (e) => {
-    e.preventDefault();
-    const formItem = e.target;
+    const runForm = (e) => {
+        e.preventDefault();
+        const formItem = e.target;
 
-    // console.log(e);
-    console.log(formItem);
+        // console.log(e);
+        console.log(formItem);
 
-    for (let i = 0; i < formItem.length; i++) {
-        const element = formItem[i].value;
-        console.log(element);
-        // data.push(element);        
+        for (let i = 0; i < formItem.length; i++) {
+            const element = formItem[i].value;
+            console.log(element);
+            // data.push(element);        
+        }
+    };
+
+    if (form) {
+        form.addEventListener('submit', runForm);
     }
-};
 
-if (form) {
-    form.addEventListener('submit', runForm);
-}
+
+    // use a API on the about page 
+
+    const page = document.getElementById('aboutPage');
+
+
+    async function runApp() {
+        const res = await fetch('https://official-joke-api.appspot.com/jokes/ten');
+        const data = await res.json();
+
+        const test = data.map(loadAll);
+
+        function cardItem(e) {
+            const card = document.createElement('div');
+            const header = document.createElement('h4');
+            const p = document.createElement('p');
+
+            card.classList.add('card');
+            header.innerHTML = 'Joke';
+
+            p.innerHTML = e.setup;
+
+            card.appendChild(header);
+            card.appendChild(p);
+            page.appendChild(card);
+        }
+        
+        function loadAll(e) {
+            console.log(e);
+            cardItem(e);
+        }
+
+    }
+
+    if (page) {
+        runApp();
+    }
 
    
 };
