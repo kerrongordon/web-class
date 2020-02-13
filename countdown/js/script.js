e8f795359d087b74422b98a45061a490edd7df53
kerrongordon <kgpsounds.com@gmail.com>
2020-02-13 14:47:36 -0400

    digital clock

--- countdown/js/script.js
+++ countdown/js/script.js
@@ -3,10 +3,20 @@ const numcount = document.getElementById('numcount');
 
 setInterval(() => {
 
-    numcount.innerHTML = timer = timer - 1;
+    let date = new Date();
+    let hours = addZerotofront(date.getHours());
+    let minutes = addZerotofront(date.getMinutes());
+    let seconds = addZerotofront(date.getSeconds());
 
-    if (timer <= 0) {
-        timer = 60;
-    }
+    numcount.innerHTML = `${hours}:${minutes}:${seconds}`;
 
 }, 1000);
+
+
+
+function addZerotofront(number) {
+    if (number.toString().length == 1) {
+        return number = '0' + number;
+    }
+    return number;
+}
